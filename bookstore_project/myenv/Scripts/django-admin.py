10790c6546e809bd9abdc74926a2ed5cc3ce550e
azzebi oussema <oussema.azzebi@gmail.com>
2020-04-26 19:12:47 +0200

    remove myenv folder

--- bookstore_project/myenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\DELL\book_store_project\bookstore_project\myenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
