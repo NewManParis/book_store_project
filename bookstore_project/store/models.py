e6245e62b2581ec67c66f4a67bc867c2a3a26815
azzebi oussema <oussema.azzebi@gmail.com>
2020-04-27 21:53:35 +0200

    update models

--- bookstore_project/store/models.py
+++ bookstore_project/store/models.py
@@ -1,41 +1,81 @@
 from django.db import models
 from django.utils import timezone
+from enum import Enum 
 
 # Create your models here.
 
 class Author(models.Model):
-	name = models.CharField("author's name", max_length = 100)
+	name = models.CharField('name', max_length=200, unique=True)
 
-	class meta:
-		pass
+	class Meta:
+		verbose_name = "author"
+
+	def __str__(self):
+		return self.name
 
 
 class Book(models.Model):
-	reference = models.IntegerField("book's reference", default=0)
-	created_at = models.DateTimeField("book's creation date")
-	available = models.BooleanField("available or not", default=True)
-	title = models.CharField("book's title", max_length = 100)
-	category = models.CharField("book's category", max_length = 100)
-	picture = models.URLField("book's url picture", max_length = 200)
-	price = models.IntegerField("book's price", default=0)
-	authors = models.ManyToManyField(Author)
 
-	class meta:
-		pass
+	class STATUS(Enum):
+		available = (1, 'Available to borrow')
+		borrowed = (2, 'Borrowed by someone')
+		archived = (3, 'Archived - not available anymore')
+
+		@classmethod
+		def get_value(cls, member):
+			return member.value[0]
+
+	class CATEGORY(Enum):
+		other = ('other', 'Other')
+		astronomy = ('astronomy', 'Astronomy')
+		biology = ('biology', 'Biology')
+		chemistry = ('chemistry', 'Chemistry')
+		history = ('history', 'History')
+		medecine = ('biology', 'Biology')
+		mathematics = ('mathematics', 'Mathematics')
+		psychology = ('psychology', 'Psychology')
+
+		@classmethod
+		def get_value(cls, member):
+			return cls[member].value[0]
+
+	reference = models.IntegerField('reference', null=True)
+	created_at = models.DateTimeField('creation date',  auto_now_add=True)
+	status = models.CharField('status', max_length=200, choices=[x.value for x in STATUS], default=STATUS.get_value(STATUS.available))
+	title = models.CharField('title', max_length=200)
+	category = models.CharField('category', max_length=200, choices=[x.value for x in CATEGORY], default=CATEGORY.get_value('other'))
+	picture = models.TextField('picture URL') 
+	price = models.IntegerField('price', null=True)
+	authors = models.ManyToManyField(Author, related_name='books', blank=True)
+
+	class Meta:
+		verbose_name = "book"
+
+	def __str__(self):
+		return self.title
+
 
 class User(models.Model):
-	user_name = models.CharField("user's name", max_length = 100)
-	user_email = models.CharField("user's email", max_length = 100)
+	user_name = models.CharField('contact name', max_length=200)
+	user_email = models.EmailField('contact email', max_length=100)
 
-	class meta:
-		pass 
+	class Meta:
+		verbose_name = "user" 
+
+	def __str__(self):
+		return self.user_name
+
 
 class Booking(models.Model):
-	created_at = models.DateTimeField("booking creation date")
-	processed = models.BooleanField("is processed ?", default= False)
+	created_at = models.DateTimeField('creation date', auto_now_add=True)
+	processed = models.BooleanField('is processed ?', default= False)
 	book = models.OneToOneField(Book, on_delete=models.CASCADE)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-	class meta:
-		pass
+	class Meta:
+		verbose_name = "booking" 
+
+	def __str__(self):
+		return self.user.name
+
 
