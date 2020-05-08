eacb0659d34d8e52e672b76e822d9b3b79fb2d79
azzebi oussema <oussema.azzebi@gmail.com>
2020-05-09 01:03:07 +0200

    update settings.py

--- bookstore_project/bookstore_project/settings.py
+++ bookstore_project/bookstore_project/settings.py
@@ -146,10 +146,6 @@ if os.environ.get('ENV') == 'PRODUCTION':
     # Simplified static file serving.
     # https://warehouse.python.org/project/whitenoise/
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-    #db_from_env = dj_database_url.config(conn_max_age=500)
-    #DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-    #DATABASES['default'].update(db_from_env)
-
+    
 # Activate Django-Heroku.
 django_heroku.settings(locals())
