6d06552fa3a05ff2e9a5c70c0d21182883a83568
azzebi oussema <oussema.azzebi@gmail.com>
2020-05-11 01:18:11 +0200

    add connexion view function

--- bookstore_project/store/views.py
+++ bookstore_project/store/views.py
@@ -6,7 +6,27 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db import transaction, IntegrityError
 from store.choices import * 
 
+from django.contrib.auth import authenticate, login
+
 # Create your views here.
+def connexion(request):
+    error = False
+
+    if request.method == "POST":
+        form = ConnexionForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)  # Nous vérifions si les données sont correctes
+            if user:  # Si l'objet renvoyé n'est pas None
+                login(request, user)  # nous connectons l'utilisateur
+            else: # sinon une erreur sera affichée
+                error = True
+    else:
+        form = ConnexionForm()
+
+    return render(request, 'connexion.html', locals())
+
 def index(request):
     books = Book.objects.filter(status=1).order_by('-created_at')[:12]
     context = {
