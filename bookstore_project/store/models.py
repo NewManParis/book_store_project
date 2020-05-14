cb3e21536b22df67a97c160a1d4a5bd156e5564d
azzebi oussema <oussema.azzebi@gmail.com>
2020-05-14 22:04:36 +0200

    add profil model

--- bookstore_project/store/models.py
+++ bookstore_project/store/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from store.choices import * 
 from django import forms
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here
 class Author(models.Model):
@@ -45,7 +47,7 @@ class Booking(models.Model):
 	def __str__(self):
 		return self.user.name
 
-'''class Profil(models.Model):
+class Profil(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)  # OneToOne to User model
 	web_site = models.URLField(blank=True)
 	#avatar = models.ImageField(null=True, blank=True, upload_to="avatars/")
@@ -53,4 +55,13 @@ class Booking(models.Model):
 	subscribed_newsletter = models.BooleanField(default=False)
 
 	def __str__(self):
-		return "Profil of {0}".format(self.user.username)'''
+		return "Profil of {0}".format(self.user.username)
+	
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
